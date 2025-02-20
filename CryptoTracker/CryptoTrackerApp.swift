881b82f1e27bf350a1f2e2c12bbdda0fb312cdd6
Din <Din.Salehy@triosstudent.com>
2025-02-20 11:09:50 -0500

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Din Salehy on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
